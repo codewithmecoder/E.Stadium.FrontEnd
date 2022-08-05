fb2da26825e0cefc7284f2ed3da545635c8fcee8
cheanith2909 <sokcheanith1031@hotmail.com>
2022-08-05 08:28:19 +0700

    build: restucture

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import LandingPage from './components/LandingPage';
+import LandingPage from '../components/LandingPage';
 
 const Home: NextPage = () => {
   return (
